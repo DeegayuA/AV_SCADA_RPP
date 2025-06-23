bf296873dbd41b6a9648e3e8d3e89784652b6144
Deeghayu Adhikari <63475473+DeegayuA@users.noreply.github.com>
2025-06-23 15:35:40 +0530

    .

--- config/constants.ts
+++ config/constants.ts
@@ -30,7 +30,7 @@ export const PLANT_TYPE = "Solar Power Plant";
 export const PLANT_CAPACITY = "2000 kW"; // 2 MW
 export const PLANT_CAPACITY_WATTS = 2000000; // 2 MW in watts
 
-export const APP_NAME = "Synergy Power - Ranna 2MW";
+export const APP_NAME = "Mini Grid - AVR&D";
 export const APP_BASE_URL = "https://av-mini-grid-offline-dashboard.vercel.app"; 
 export const APP_URL = "https://yourwebsite.com";
 export const APP_KEYWORDS = "solar, monitoring, control, energy, management";
